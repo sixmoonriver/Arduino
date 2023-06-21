3a6df75859cd2c5a3f9a61762aee182303e78929
sixmoonriver <yaofayang@qq.com>
2023-06-21 17:03:21 +0800

    调整了舵的角度

--- test/NR24L01/boatControl/boatreciver.ino
+++ test/NR24L01/boatControl/boatreciver.ino
@@ -27,7 +27,7 @@ void setup()
     Mirf.spi = &MirfHardwareSpi;
     Mirf.init();
     Mirf.setRADDR((byte *)"RECVE"); //设置自己的地址（发送端地址），使用5个字符
-    Mirf.payload = 24;
+    Mirf.payload = 4;  // 设置传送位数，16位是2，32位是4；
     Mirf.channel = 90;              //设置所用信道
     Mirf.config();
   Serial.begin(9600);
@@ -36,13 +36,14 @@ void setup()
   Mirf.readRegister( RF_SETUP, &rf_setup, sizeof(rf_setup) );
   Serial.print( "rf_setup = " );
   Serial.println( rf_setup, BIN );
-  mastermotor.attach(6);  // 设置主电机的控制脚为3，arduino 上只能是3 5 6 9 10 11这几个支持PWM输出的引脚。
+  mastermotor.attach(6);  // 设置主电机的控制脚为6，arduino 上只能是3 5 6 9 10 11这几个支持PWM输出的引脚。
   //以下为电调初始化，首先把油门行程设置为最大，延迟2秒后，把油门行程设置为最小。
   mastermotor.writeMicroseconds(2000);
   delay(2000);
   mastermotor.writeMicroseconds(10);
   fxservo.attach(5); // 设置方向舵机的控制引脚为5；
-  camservo.attach(3); // 设置摄像头舵机的控制引脚为6；
+  fxservo.write(90); // 舵板初始化到中间的位置；
+  camservo.attach(3); // 设置摄像头舵机的控制引脚；
   Serial.println("Init OK!");
 }
  
@@ -50,20 +51,24 @@ void loop()
 {
    if(Mirf.dataReady()) {  //当接收到程序，便从串口输出接收到的数据
     Mirf.getData((byte *) &value);
-  fx = highByte(value);
-  ym = lowByte(value);
-  con_value = (value>>16) & 0xff;
-    Serial.print(highByte(value));
-    Serial.print(",");
-  Serial.println(lowByte(value));
+  //fx = highByte(value);
+  //ym = lowByte(value);
+  fx = (value & 0xff00) >> 8;
+  ym = value & 0xff;
+  con_value = (value & 0xffff0000) >>16;
+  Serial.print("fx = ");
+  Serial.print(fx);
+  Serial.print(",");
+  Serial.print("ym = ");
+  Serial.println(ym);
   Serial.print("Switch and CAM: ");
   Serial.println(con_value,BIN);
-    delay(100);
+  delay(100);
  }
   //转换油门值为油门实际值，进行油门控制
-  motorspeed = map(ym, 130, 0, 1000,2000); 
-    mastermotor.writeMicroseconds(motorspeed); 
-  fxval = map(fx,0,255,45,135); //转换方向舵的值；
+  motorspeed = map(ym, 130, 255, 1000,2000); 
+  mastermotor.writeMicroseconds(motorspeed); 
+  fxval = map(fx,0,255,60,120); //转换方向舵的值； 控制舵的偏转范围，45-->60 
   fxservo.write(fxval);
   //控制数据处理
   //1、摄像头方向控制
