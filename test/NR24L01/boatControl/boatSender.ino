f790b1643646b7df5331bc9a0b974619f001527b
sixmoonriver <yaofayang@qq.com>
2023-10-15 15:33:02 +0800

    2023年10月15日中特威电调，A2212电机，9克舵机调试通过
    1、修改了一些变量的定义，避免数据溢出的；
    2、修改了引脚的接法，避免了模拟引脚重复；
    3、修改了payload设置，改为4，传输32位的数据；
    4、修改了读取数据10位 ，而存储变量为8位的Bug;
    5、修改了摄像头范围值16->15，避免4位数据保存溢出的问题；
    6、修改了传送数据的部分，改到条件判断成立后，增加了变量复位的功能，避免垃圾数据的干扰。

--- test/NR24L01/boatControl/boatSender.ino
+++ test/NR24L01/boatControl/boatSender.ino
@@ -17,16 +17,17 @@
 #include <MirfHardwareSpiDriver.h>
 uint8_t convert[5]={0};
 uint8_t cam_value= 0;
-uint8_t x_value,y_value,last_xvalue,last_yvalue,con_value,last_convalue;
-uint16_t trans_value = 0;
+uint16_t xSourceValue,ySourceValue,camSourceValue,temp_value=0;
+uint8_t x_value,y_value,last_xvalue,last_yvalue,con_value,last_convalue=0;
+uint32_t trans_value = 0;
 int xpin = 0;  // 方向输入脚
 int ypin = 1;  // 油门输入脚
 //开关引脚定义
-int switchPin1 = 2;
-int switchPin2 = 3;
-int switchPin3 = 4;
-int switchPin4 = 5;
-int camControl = 8;
+int switchPin1 = 6;
+int switchPin2 = 7;
+int switchPin3 = 8;
+int switchPin4 = 9;
+int camControl = 2;
 //开关状态
 int pin1Reading,pin2Reading,pin3Reading,pin4Reading = 0;
 int switchState1,switchState2,switchState3,switchState4 = 0;
@@ -38,7 +39,7 @@ void setup()
   Mirf.spi = &MirfHardwareSpi;
   Mirf.init();
   Mirf.setRADDR((byte *)"SENDE"); //设置自己的地址（发送端地址），使用5个字符
-  Mirf.payload = 24;     //设置传送位数，每通道使用8位；低8位是油门、次8位是方向，再高位是功能开关。
+  Mirf.payload = 4;     //设置传送位数，每通道使用8位；低8位是油门、次8位是方向，再高位是功能开关。
   Mirf.channel = 90;              //设置所用信道
   Mirf.config();
   Serial.begin(9600);
@@ -52,14 +53,14 @@ void loop()
 {
   Mirf.setTADDR((byte *)"RECVE");           //设置接收端地址
   //读取摇杆的数值
-  x_value = analogRead(xpin);  //读取摇杆输入的数值
-  x_value = map(x_value,0,1023,0,255); //将模拟量的10位值转为8位值；
-  y_value = analogRead(ypin);
-  y_value = map(y_value,0,1023,0,255);
+  xSourceValue = analogRead(xpin);  //读取摇杆输入的数值
+  x_value = map(xSourceValue,0,1023,0,255); //将模拟量的10位值转为8位值；
+  ySourceValue = analogRead(ypin);
+  y_value = map(ySourceValue,0,1023,0,255);
   //value = random(255); 
   //读取摄像头角度控制值
-  cam_value = analogRead(camControl);
-  cam_value = map(cam_value,0,1023,0,16);
+  camSourceValue = analogRead(camControl);
+  cam_value = map(camSourceValue,0,1023,0,15);
   //读取开关1状态，如果状态发生变化，记录时间
   pin1Reading = digitalRead(switchPin1);
   if( lastSwitchState1 != switchState1) {
@@ -73,16 +74,31 @@ void loop()
   }
   // 控制数据处理
   // 最高4位为开关状态，最高位为开关1的状态，以此类推6、5、4为开关2、3、4
-  bitWrite(con_value,7,switchState1);
+  //bitWrite(con_value,7,switchState1);
   lastSwitchState1 = pin1Reading;
   //开关2~4位处理；
   //控制数据组装
-  con_value = con_value + cam_value;
-  //发送的数据组装
-  trans_value = (con_value<<16) + (x_value<<8) + y_value;  
-  if(x_value != last_xvalue | y_value != last_yvalue)
+  con_value =  cam_value;
+  
+  
+  if(x_value != last_xvalue | y_value != last_yvalue | con_value!= last_convalue)
   {
-    Mirf.send((byte *)&trans_value);                //发送指令，组合后的数据，低8位为油门数据，高8位为方向数据
+    //发送的数据组装
+	trans_value = 0;
+	if(con_value > 0){
+	    trans_value = con_value + trans_value;
+		trans_value = trans_value<<16; //控制数据左移16位，到16~24位；	
+	}
+	if(x_value > 0){
+		temp_value = 0;
+		temp_value = (temp_value + x_value) << 8;
+		trans_value =  temp_value + trans_value; //方向数据左移8位，8~16位；	
+	}
+    if(y_value > 0){
+		trans_value = trans_value + y_value; //油门数据组装
+	}
+ 
+	Mirf.send((byte *)&trans_value);                //发送指令，组合后的数据，低8位为油门数据，高8位为方向数据
     while(Mirf.isSending()) delay(1);          //直到发送成功，退出循环
     last_xvalue = x_value;   //last数据更新
     last_yvalue = y_value;
@@ -91,6 +107,8 @@ void loop()
     Serial.println(x_value);
     Serial.print("y_value: ");
     Serial.println(y_value);
+	Serial.print("cam_value: ");
+    Serial.println(cam_value);
     Serial.print("trans_value: ");
     Serial.println(trans_value);
   }
